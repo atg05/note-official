3df742c516c9d14c70b25260489f1214c4f61ce7
Avinash Kumar <itsmeatg@outlook.com>
2023-11-29 23:11:27 +0530

    Editor

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import "./Editor.style.css";
 const Editor = () => {
-  return <div className="editor_container">Editor</div>;
+  return (
+    <div className="editor_container">
+      <input type="text" placeholder="Title" className="title_input" />
+      <textarea placeholder="Start typing..." />
+    </div>
+  );
 };
 
 export default Editor;
